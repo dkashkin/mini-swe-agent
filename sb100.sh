2830b85da2b3082fe18fa4cb260bbdcbb08742ae
Dennis Kashkin <kashkin@gmail.com>
2025-09-05 13:23:03 -0500

    fix

--- sb100.sh
+++ sb100.sh
@@ -5,7 +5,7 @@ mini-extra swebench \
 --subset verified \
 --split test \
 --model "vertex_ai/gemini-2.5-pro" \
---workers 100 \
+--workers 20 \
 --slice "0:100" \
 --environment-class docker \
 --output "out" \
