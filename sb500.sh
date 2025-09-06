5646286cd50da97b614e3fe675362cd65affbffe
Dennis Kashkin <kashkin@gmail.com>
2025-09-05 20:24:41 -0500

    increase workers to 30

--- sb500.sh
+++ sb500.sh
@@ -5,7 +5,7 @@ mini-extra swebench \
 --subset verified \
 --split test \
 --model "vertex_ai/gemini-2.5-pro" \
---workers 20 \
+--workers 30 \
 --environment-class docker \
 --output "out" \
 --run-id "$1" \
