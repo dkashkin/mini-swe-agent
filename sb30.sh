70f44bb86260569ebf28ef48be77ffb146092518
Dennis Kashkin <kashkin@gmail.com>
2025-09-08 15:57:49 -0500

    add sb30.sh

---
+++ sb30.sh
@@ -0,0 +1,14 @@
+mkdir -p out && 
+zip -rq out_backup_$(date +%m%d%y_%H%M).zip out && 
+rm -rf out/* &&
+mini-extra swebench \
+--subset verified \
+--split test \
+--model "vertex_ai/gemini-2.5-pro" \
+--workers 30 \
+--slice "0:30" \
+--environment-class docker \
+--output "out" \
+--run-id "$1" \
+--redo-existing \
+--shuffle
