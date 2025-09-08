6ce5de3733f7c2377aeee5fb144460f97fe757c0
Dennis Kashkin <kashkin@gmail.com>
2025-09-08 16:10:44 -0500

    add sonnet

---
+++ sb1sonnet.sh
@@ -0,0 +1,13 @@
+mkdir -p out && 
+zip -rq out_backup_$(date +%m%d%y_%H%M).zip out && 
+rm -rf out/* &&
+mini-extra swebench \
+--subset verified \
+--split test \
+--model "vertex_ai/claude-sonnet-4" \
+--workers 1 \
+--filter "astropy__astropy-7166" \
+--environment-class docker \
+--output "out" \
+--redo-existing \
+--run-id "$1"
