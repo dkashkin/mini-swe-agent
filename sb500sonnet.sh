853701262618b93c0c2249d8976589ed82b046a3
Dennis Kashkin <kashkin@gmail.com>
2025-09-09 22:18:31 -0500

    add sonnet

---
+++ sb500sonnet.sh
@@ -0,0 +1,12 @@
+mkdir -p out && 
+zip -rq out_backup_$(date +%m%d%y_%H%M).zip out && 
+rm -rf out/* &&
+mini-extra swebench \
+--subset verified \
+--split test \
+--model "vertex_ai/claude-sonnet-4" \
+--workers 30 \
+--environment-class docker \
+--output "out" \
+--run-id "$1" \
+--shuffle
